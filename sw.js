89a441602ac00d8df1b27ec0194e75284d0eea0b
Bruno Alencar <bruno.idealencar@gmail.com>
2018-07-30 23:42:21 +0000

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-0a6b552cb6206a13b611.js","e58c038f0b9049c27e3ce00a5d96d415"],["/app-5b8ebe5494b98ae1bce3.js","95620186c5a0710cb19eb3ec586891c9"],["/app-ec5418a301edf614db37.js","499a37f283648a47a134bf40e6683c0c"],["/commons-0cb8f724c7f4a701e472.js","d7ddc869338f724f2e3eeeaeb8d1361b"],["/commons-2266b06f71f96b98d556.js","fdcc5cd86d88f18e7f39d9ff6055ea10"],["/commons-8d8007a17ba04c9f4039.js","458f53758b39685799ea522061ad5c87"],["/commons-96cb13ddc2e4f801582c.js","3578725c175d0599a89e5e66185a00e5"],["/index.html","024a3e35b2e531d7e3cb4469509dd818"],["/manifest.webmanifest","33cdd9ddb6f43737c5e77451c28354a2"],["/offline-plugin-app-shell-fallback/index.html","ccaca8721c129d56a26864fa3cf10cbb"]];
+var precacheConfig = [["/app-0a6b552cb6206a13b611.js","e58c038f0b9049c27e3ce00a5d96d415"],["/app-5b8ebe5494b98ae1bce3.js","95620186c5a0710cb19eb3ec586891c9"],["/app-ec5418a301edf614db37.js","499a37f283648a47a134bf40e6683c0c"],["/commons-0cb8f724c7f4a701e472.js","d7ddc869338f724f2e3eeeaeb8d1361b"],["/commons-2266b06f71f96b98d556.js","fdcc5cd86d88f18e7f39d9ff6055ea10"],["/commons-8d8007a17ba04c9f4039.js","458f53758b39685799ea522061ad5c87"],["/commons-96cb13ddc2e4f801582c.js","3578725c175d0599a89e5e66185a00e5"],["/index.html","b58c5b9c286667c51809bdfef8b9c310"],["/manifest.webmanifest","33cdd9ddb6f43737c5e77451c28354a2"],["/offline-plugin-app-shell-fallback/index.html","65ff892b69aa15f5e09b3a993da188b6"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
